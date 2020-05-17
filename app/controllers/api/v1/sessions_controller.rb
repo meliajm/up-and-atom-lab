66a01504fa805857e9dc5b1b12ee67b9bd5772e1
github username <meliajm@gmail.com>
2020-05-16 20:43:18 -0700

    remove binding.pry

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -4,7 +4,6 @@ class Api::V1::SessionsController < ApplicationController
     end
 
     def create
-        binding.pry
         user = User.find_by(email: params[:session][:email])
         if user && user.authenticate(params[:session][:password])
             session[:user_id] = user.id 
